d539faf580b2e03ba7882b7c7003bb7187da513f
jopich <rodion_boldyrev05@vk.com>
2022-10-06 09:13:07 +0300

    Кол-во дней

--- DateTime.pas
+++ DateTime.pas
@@ -3,9 +3,18 @@
   var b:=false;
   if ((r mod 4=0) and (r mod 100<>0)) or (r mod 400=0) then
     b:=true;
-  println(readinteger()*60);
+  Print('Высокосноть',b);
+  println('Секунды',readinteger()*60);
   if (b=true) then
     Print(366)
   else
     Print(365);
+  var sum:=0;
+  var n:=readinteger;
+  for var i:=r to n do
+    if ((i mod 4=0) and (i mod 100<>0)) or (i mod 400=0) then
+      sum+=366
+    else
+      sum+=365;
+  Print('Дней',sum);
 end.
