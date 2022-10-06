c14704886f73a73228ecc8171e326286b85908d8
jopich <rodion_boldyrev05@vk.com>
2022-10-06 09:10:34 +0300

    сколько дней в году

--- DateTime.pas
+++ DateTime.pas
@@ -1,8 +1,11 @@
 ﻿begin
   var r:=readinteger;
+  var b:=false;
   if ((r mod 4=0) and (r mod 100<>0)) or (r mod 400=0) then
-    Println('True')
-  else
-    Println('False');
+    b:=true;
   println(readinteger()*60);
+  if (b=true) then
+    Print(366)
+  else
+    Print(365);
 end.
