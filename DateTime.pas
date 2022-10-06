05d663fe2055f954c3a2f6794930c131b6106752
jopich <rodion_boldyrev05@vk.com>
2022-10-06 08:59:13 +0300

    'seconds'

--- DateTime.pas
+++ DateTime.pas
@@ -1,3 +1,8 @@
 ﻿begin
- Print('hello world');
+  var r:=readinteger;
+  if ((r mod 4=0) and (r mod 100<>0)) or (r mod 400=0) then
+    Println('True')
+  else
+    Println('False');
+  println(readinteger()*60);
 end.
